aa07d88449f6e7972d5f93ddf4a38994533bd471
ilyafilim <ilya.filim@gmail.com>
2020-03-14 22:45:36 +0300

    spaced

--- src/main/java/ru/ilyafilim/Main.java
+++ src/main/java/ru/ilyafilim/Main.java
@@ -14,6 +14,7 @@ public class Main {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("/spring-context.xml");
         QuestionService questionService = context.getBean(QuestionService.class);
+
         Scanner in = new Scanner(System.in);
         System.out.print("Добро пожаловать, пожалуйста введите своё Имя: ");
         String name = in.nextLine();
@@ -21,6 +22,7 @@ public class Main {
         String surname = in.nextLine();
         System.out.println("Отлично, начинаем тест, первый вопрос:");
         Question question; int c = 0;
+
         for (int a = 0; a != Statics.MAX_QUESTION; a++) {
             question = questionService.getNextQuestion();
             System.out.print("\n" + (a + 1) + ". " + question.getDefinition() + "\nВаш ответ: ");
@@ -32,6 +34,7 @@ public class Main {
                 System.out.println("К сожалению, это неверный ответ, правильный - " + question.getTerm() + ". И так, " + (Statics.MAX_QUESTION - a == 1 ? "Финальный вопрос:" : "Следующий вопрос:"));
             }
         }
+
         System.out.println();
         System.out.println("Тест окончен. Спасибо за прохождение, " + name + " " + surname + ". Вы набрали " + c + " " + (c > 4 ? "баллов" : c > 1 ? "балла" : c == 1 ? "балл" : "баллов") + ".");
         context.close();
