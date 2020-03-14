055a891faf0ee973766a34d399fb5463cf4da80c
ilyafilim <ilya.filim@gmail.com>
2020-03-14 22:51:43 +0300

    add spaces

--- src/main/java/ru/ilyafilim/constants/Statics.java
+++ src/main/java/ru/ilyafilim/constants/Statics.java
@@ -2,6 +2,7 @@ package ru.ilyafilim.constants;
 
 public class Statics {
 
+
     public final static int MAX_QUESTION = 4;
     public static int question = 0;
 
--- src/main/java/ru/ilyafilim/service/QuestionService.java
+++ src/main/java/ru/ilyafilim/service/QuestionService.java
@@ -3,5 +3,6 @@ package ru.ilyafilim.service;
 import ru.ilyafilim.domain.Question;
 
 public interface QuestionService {
+
     Question getNextQuestion();
 }
