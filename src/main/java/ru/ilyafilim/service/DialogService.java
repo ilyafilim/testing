2e05f33a06a308da215d6fc528b98b08b9461831
ilyafilim <ilya.filim@gmail.com>
2020-03-24 20:39:09 +0300

    fix

---
+++ src/main/java/ru/ilyafilim/service/DialogService.java
@@ -0,0 +1,13 @@
+package ru.ilyafilim.service;
+
+public interface DialogService {
+    String getHello();
+    String enterName();
+    String enterSurname();
+    String letsTest();
+    String getRight();
+    String getWrong();
+    String nextQuestion();
+    String finalQuestion();
+
+}
